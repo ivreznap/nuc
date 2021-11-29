bde45b695b3e98166e4be2a3ad7db9706bfc96fc
ivreznap <68358383+ivreznap@users.noreply.github.com>
2021-11-29 21:48:46 +0600

    Update nuc.sh

--- nuc.sh
+++ nuc.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 nuclei --update-templates --silent
 
 read -p "Enter domain names seperated by 'space' : " input
@@ -14,46 +13,37 @@ Scan started for $i
 
 mkdir $i
 
-subfinder -d $i | httpx >> $i/subdomains.txt >/dev/null
+subfinder -d $i | httpx >> $i/subdomains.txt
 
-echo "Subdomains saved at $i/subdomains.txt." | notify
+echo "subdomains saved at $i/subdomains.txt." | notify
 
-echo "Scan for CVES started."
-nuclei -l $i/subdomains.txt -t cves/ -o $i/cves.txt
-echo "Scan for CVES completed." | notify -silent
-
-echo "Scan for Default-Logins started."
-nuclei -l $i/subdomains.txt -t default-logins/ -o $i/default-logins.txt
-echo "Scan for default-logins completed." | notify -silent
-
-
-echo "Scan for Exposures started."
-nuclei -l $i/subdomains.txt -t exposures/ -o $i/exposures.txt
-echo "Scan for exposures completed." | notify -silent
-
-
-echo "Scan for Misconfigurations started."
-nuclei -l $i/subdomains.txt -t misconfiguration/ -o $i/misconfiguration.txt
-echo "Scan for misconfigurations completed." | notify -silent
-
-
-echo "Scan for takeovers started."
-nuclei -l $i/subdomains.txt -t takeovers/ -o $i/takeovers.txt
-echo "Scan for takeovers completed." | notify -silent
-
-
-
-nuclei -l $i/subdomains.txt -t vulnerabilities/ -o $i/vulnerabilities.txt
-echo "Scan for vulnerabilities completed." | notify -silent
+echo "Scan for CVES started." | notify
+nuclei -l $i/subdomains.txt -t cves/ -o $i/cves.txt | notify
+echo "Scan for CVES completed." | notify
+echo "Scan for default-logins started." | notify
+nuclei -l $i/subdomains.txt -t default-logins/ -o $i/default-logins.txt | notify
+echo "Scan for default-logins completed." | notify
+echo "Scan for exposures started." | notify
+nuclei -l $i/subdomains.txt -t exposures/ -o $i/exposures.txt | notify
+echo "Scan for exposures completed." | notify
+echo "Scan for misconfigurations started." | notify
+nuclei -l $i/subdomains.txt -t misconfiguration/ -o $i/misconfiguration.txt | notify
+echo "Scan for misconfigurations completed." | notify
+echo "Scan for takeovers started." | notify
+nuclei -l $i/subdomains.txt -t takeovers/ -o $i/takeovers.txt | notify
+echo "Scan for takeovers completed." | notify
+echo "Scan for vulnerabilities started." | notify
+nuclei -l $i/subdomains.txt -t vulnerabilities/ -o $i/vulnerabilities.txt | notify
+echo "Scan for vulnerabilities completed." | notify
 
 
 cd $i
 
-grep -r "info" > info.txt | notify -data info.txt -bulk
-grep -r "low" > low.txt | notify -data low.txt -bulk
-grep -r "medium" > medium.txt | notify -data medium.txt -bulk
-grep -r "high" > high.txt | notify -data high.txt -bulk
-grep -r "critical" > critical.txt | notify -data critical.txt -bulk
+grep -r "info" >> info.txt ; notify -data info.txt -bulk
+grep -r "low" >> low.txt ; notify -data low.txt -bulk
+grep -r "medium" >> medium.txt ; notify -data medium.txt -bulk
+grep -r "high" >> high.txt ; notify -data high.txt -bulk
+grep -r "critical" >> critical.txt ; notify -data critical.txt -bulk
 
 echo "
 .
